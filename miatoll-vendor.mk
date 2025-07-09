cb0de141379ad07c48e7ad193873bf1a34c71fe4
basamaryan <basam.aryan@gmail.com>
2025-07-09 09:01:58 -0400

    miatoll: Update WFD system blobs from dada OS2.0.217.0.WOCMIXM
    Change-Id: I58ede53f3f6e8ec88f8aaecca453c985d792bff6

--- miatoll-vendor.mk
+++ miatoll-vendor.mk
@@ -886,6 +886,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.diaghal@1.0 \
     vendor.qti.hardware.fm@1.0 \
     vendor.qti.hardware.wifidisplaysession@1.0 \
+    vendor.qti.hardware.wifidisplaysession_aidl-V1-ndk \
     vendor.qti.imsrtpservice@3.0 \
     CACertService \
     CneApp \
@@ -959,7 +960,7 @@ PRODUCT_PACKAGES += \
     wifidisplayhalservice \
     xtra-daemon \
     dpmd \
-    wfdservice64
+    wfdservice
 
 PRODUCT_PACKAGES += \
     system_ext_priv-app_WfdService_lib_arm64_libwfdnative_so \
